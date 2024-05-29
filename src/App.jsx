d97d009ed0d9fba4a8c23b76f35d3bb6f68db492
Your Name <you@example.com>
2024-05-29 16:37:11 +0700

    feat

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
 import "./App.css";
 
-function App() {
+function Tahhead() {
   return (
     <div>
-      {/*
-        Todo: 1 ให้ Declare ตัว Component ย่อย 
-       เพื่อแบ่งสัดส่วนของ HTML Element ในส่วนของ <header> 
-      */}
       <header>
         <nav>
           <div className="logo">🚀 XYZ CORP</div>
@@ -26,11 +22,13 @@ function App() {
           </ul>
         </nav>
       </header>
+    </div>
+  );
+}
 
-      {/*
-        Todo 2: ให้ Declare ตัว Component ย่อย
-        เพื่อแบ่งสัดส่วนของ HTML Element ในส่วนของ <main> 
-      */}
+function Tahmani() {
+  return (
+    <div>
       <main>
         <div className="company-detail-left">
           <h1>Welcome to XYZ Corp</h1>
@@ -60,61 +58,60 @@ function App() {
           <button className="learn-button">Learn More</button>
         </div>
       </main>
+    </div>
+  );
+}
 
-      {/*
-        Todo 3: ให้ Declare ตัว Component ย่อย
-        เพื่อแบ่งสัดส่วนของ HTML Element ในส่วนของ <section className="product-section"> 
-      */}
-      <section className="product-section">
-        <div className="product-section-container">
-          <h2 className="product-featured-heading">Featured Products</h2>
-          <div className="product-card-list">
-            <div className="product-card">
-              <img
-                src="https://placehold.co/300x300"
-                alt="The Lorem Ipsum for photos."
-              />
-              <h3>Premium Bluetooth Headphones</h3>
-              <p className="product-price">2,400 Baht</p>
-              {/*
-                Todo 5: ให้ Declare ตัว Component Button เพื่อ Render ปุ่ม “Add to Cart” ที่ซ้ำกันอยู่
-                (จุดที่ 1)
-              */}
-              {/*
-                Todo 6: หลังกจากทำ Todo 5 เสร็จ
-                ให้เปลี่ยน Style ของปุ่ม “Add to Card” แก้ไข className ใน Component `Button` ที่สร้างขึ้นมา
-                จาก “add-to-cart-button” เป็น “add-to-cart-button-secondary”
-              */}
-              <button className="add-to-cart-button">Add to Cart</button>
-            </div>
-            <div className="product-card">
-              <img
-                src="https://placehold.co/300x300"
-                alt="The Lorem Ipsum for photos."
-              />
-              <h3>Classic Leather Watch</h3>
-              <p className="product-price">4,000 Baht</p>
-              {/* Todo 5: (จุดที่ 2) */}
-              <button className="add-to-cart-button">Add to Cart</button>
-            </div>
-            <div className="product-card">
-              <img
-                src="https://placehold.co/300x300"
-                alt="The Lorem Ipsum for photos."
-              />
-              <h3>Organic Green Tea</h3>
-              <p className="product-price">79.99 Baht</p>
-              {/* Todo 5: (จุดที่ 3) */}
-              <button className="add-to-cart-button">Add to Cart</button>
-            </div>
+function Tahproduct() {
+  return (
+    <section className="product-section">
+      <div className="product-section-container">
+        <h2 className="product-featured-heading">Featured Products</h2>
+        <div className="product-card-list">
+          <div className="product-card">
+            <img
+              src="https://placehold.co/300x300"
+              alt="The Lorem Ipsum for photos."
+            />
+            <h3>Premium Bluetooth Headphones</h3>
+            <p className="product-price">2,400 Baht</p>
+            <button className="add-to-cart-button">Add to Cart</button>
+          </div>
+          <div className="product-card">
+            <img
+              src="https://placehold.co/300x300"
+              alt="The Lorem Ipsum for photos."
+            />
+            <h3>Classic Leather Watch</h3>
+            <p className="product-price">4,000 Baht</p>
+            <button className="add-to-cart-button">Add to Cart</button>
+          </div>
+          <div className="product-card">
+            <img
+              src="https://placehold.co/300x300"
+              alt="The Lorem Ipsum for photos."
+            />
+            <h3>Organic Green Tea</h3>
+            <p className="product-price">79.99 Baht</p>
+            <button className="add-to-cart-button">Add to Cart</button>
           </div>
         </div>
-      </section>
+      </div>
+    </section>
+  );
+}
 
-      {/*
-        Todo 4: ให้ Declare ตัว Component ย่อย
-        เพื่อแบ่งสัดส่วนของ HTML Element ในส่วนของ <footer> 
-      */}
+function Tahfooter() {
+  return <div></div>;
+}
+
+function App() {
+  return (
+    <div>
+      <Tahhead />
+      <Tahmani />
+      <Tahproduct />
+      <Tahfooter />
       <footer>
         <p>&copy; 2023 Your Ecommerce Store. All rights reserved.</p>
       </footer>
